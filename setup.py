90c0c89b18ea23788a25f6ae95260294bb4df17c
Jackson Dean <jacksonsdean@gmail.com>
2023-02-22 14:55:33 -0500

    switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='evolution_torch',
-    version='0.0.8',
-    author='Jackson Dean',
-    author_email='jackson@downbeat.games',
-    description='Evolutionary algorithm implementation in PyTorch',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/jacksonsdean/evolution_torch',
-    project_urls = {
-        "Bug Tracker": "https://github.com/jacksonsdean/evolution_torch/issues"
-    },
-    license='MIT',
-    packages=['evolution_torch'],
-    install_requires=['torch', 'numpy', 'matplotlib', 'scipy', 'scikit-image', 'functorch', 'tqdm', 'torchvision', 'piq', 'imageio','scikits-bootstrap', 'networkx'],
-)
+if __name__ == "__main__":
+    setup()
